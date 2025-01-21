a60d1c67d769c487d9fc2e0190dd6f62db8f3e2f
malek-shehadeh <maleksameeh96@gmail.com>
2025-01-21 15:15:10 +0300

    add to main

--- src/components/ChatBot/types.ts
+++ src/components/ChatBot/types.ts
@@ -1,33 +1,4 @@
-// export interface Message {
-//     id: string;
-//     content: string;
-//     sender: 'user' | 'bot';
-//     timestamp: Date;
-//     quickReplies?: QuickReply[];
-//   }
-  
-//   export interface QuickReply {
-//     id: string;
-//     text: string;
-//     action: string;
-//   }
-  
-//   export interface ChatBotProps {
-//     isOpen?: boolean;
-//     onClose?: () => void;
-//   }
-//   export interface ResponseNode {
-//     content: string;
-//     quickReplies?: QuickReply[];
-//     keywords?: string[];
-//     route?: string;
-//   }
 
-
-
-///
-
-// types.ts
 export interface Message {
   id: string;
   content: string;
